13a813871497f46990a0e1d219142119128bc695
i.wiranatha <i.wiranatha@gdn-commerce.com>
2019-04-29 17:09:24 +0700

    add vue material

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
-import VueMDCAdapter from "vue-mdc-adapter";
 
-Vue.use(VueMDCAdapter);
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
+import 'vue-material/dist/theme/default.css'
+Vue.use(VueMaterial)
 
 Vue.config.productionTip = false;
 
